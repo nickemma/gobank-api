fd01279c70ff0f360a00c34120f879316ab9ed12
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-05-11 01:52:25 +0100

    Added docker image for postgres

--- storage.go
+++ storage.go
@@ -1,8 +1,14 @@
 package main
 
+import "database/sql"
+
 type Storage interface {
 	CreateAccount(account *Account) error
 	DeleteAccount(id int) error
 	UpdateAccount(account *Account) error
 	DeleteAccountByID(id int) (*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
 }
